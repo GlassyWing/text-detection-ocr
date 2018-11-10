66eb44b679a64e70b714f0f7617a37b5345242c5
GlassyWing <1490215053@qq.com>
2018-11-10 17:12:21 +0800

    添加ctpn训练类

--- ctpn_train.py
+++ ctpn_train.py
@@ -1,15 +1,45 @@
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.callbacks import EarlyStopping, TensorBoard
 
 from ctpn import CTPN
-from ctpn.lib.utils import gen_sample
-from custom import LRScheduler
+from ctpn.data_loader import DataLoader
+from ctpn.lib.utils import get_session
+from custom import LRScheduler, SingleModelCK
+import keras.backend as K
 
 if __name__ == '__main__':
-    gen = gen_sample("E:\data\VOCdevkit\VOC2007\Annotations", "E:\data\VOCdevkit\VOC2007\JPEGImages")
+    import argparse
 
-    checkpoint = ModelCheckpoint(r'model\weights-ctpnlstm-{epoch:02d}.hdf5', save_weights_only=True)
+    K.set_session(get_session(0.8))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ie", "--initial_epoch", help="初始迭代数", default=0, type=int)
+    parser.add_argument("--epochs", help="迭代数", default=20, type=int)
+    parser.add_argument("--gpus", help="gpu的数量", default=1, type=int)
+    parser.add_argument("--images_dir", help="图像位置", default="E:\data\VOCdevkit\VOC2007\JPEGImages")
+    parser.add_argument("--anno_dir", help="标注文件位置", default="E:\data\VOCdevkit\VOC2007\Annotations")
+    parser.add_argument("--config_file_path", help="模型配置文件位置",
+                        default="config/ctpn-default.json")
+    parser.add_argument("--weights_file_path", help="模型初始权重文件位置",
+                        default=None)
+
+    args = parser.parse_args()
+    config = CTPN.load_config(args.config_file_path)
+
+    weights_file_path = args.weights_file_path
+    if weights_file_path is not None:
+        config["weight_path"] = weights_file_path
+    config['num_gpu'] = args.gpus
+
+    ctpn = CTPN(**config)
+
+    data_loader = DataLoader(args.anno_dir, args.images_dir)
+
+    checkpoint = SingleModelCK(r'model\weights-ctpnlstm-{epoch:02d}.hdf5', model=ctpn.model, save_weights_only=True)
     earlystop = EarlyStopping(patience=10)
+    log = TensorBoard(log_dir='logs', histogram_freq=0, batch_size=1, write_graph=True, write_grads=False)
     lr_scheduler = LRScheduler(lambda epoch, lr: lr / 2, watch="loss", watch_his_len=1)
 
-    ctpn = CTPN(vgg_trainable=True)
-    ctpn.train(gen, epochs=20, steps_per_epoch=2, callbacks=[checkpoint, earlystop, lr_scheduler])
+    ctpn.train(data_loader.load_data(),
+               epochs=args.epochs,
+               steps_per_epoch=data_loader.steps_per_epoch,
+               callbacks=[checkpoint, earlystop, lr_scheduler])
