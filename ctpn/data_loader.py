991ee68ae04bf5d379ad12585d61a9732c04c9c3
manlier <1490215053@qq.com>
2018-11-10 22:34:56 +0800

    采用异步方式预先加载数据

--- ctpn/data_loader.py
+++ ctpn/data_loader.py
@@ -1,14 +1,17 @@
-import cv2
+import os
 from glob import glob
+from multiprocessing import Queue, cpu_count
+from multiprocessing.dummy import Pool
+
+import cv2
+import numpy as np
 
 from ctpn.lib.utils import random_uniform_num, readxml, cal_rpn, IMAGE_MEAN
-import numpy as np
-import os
 
 
 class DataLoader:
 
-    def __init__(self, anno_dir, images_dir):
+    def __init__(self, anno_dir, images_dir, parallelism=cpu_count(), cache_size=4):
         self.anno_dir = anno_dir
         self.images_dir = images_dir
         self.batch_size = 1
@@ -16,16 +19,38 @@ class DataLoader:
         # list xml
         self.xmlfiles = glob(f'{anno_dir}/*.xml')
         self.total_size = len(self.xmlfiles)
+        self.cache_size = cache_size
+        self.parallelism = parallelism
         self.__rd = random_uniform_num(self.total_size)
+        self.__data_queue = Queue(cache_size)
         self.xmlfiles = np.array(self.xmlfiles)
         self.steps_per_epoch = self.total_size // self.batch_size
+        self.__init_queue()
 
-    def load_data(self):
+    def __init_queue(self):
+        for _ in range(self.cache_size):
+            shuf = self.xmlfiles[self.__rd.get(1)]
+            self.__data_queue.put(self.__single_sample(shuf[0]))
+
+    def __produce(self):
         xmlfiles = self.xmlfiles
         while True:
             shuf = xmlfiles[self.__rd.get(1)]
-            gtbox, imgfile = readxml(shuf[0])
-            img = cv2.imread(os.path.join(self.images_dir, imgfile))
+            self.__data_queue.put(self.__single_sample(shuf[0]))
+
+    def __single_sample(self, xml_path):
+        gtbox, imgfile = readxml(xml_path)
+        img = cv2.imread(os.path.join(self.images_dir, imgfile))
+        return gtbox, imgfile, img
+
+    def load_data(self):
+        pool = Pool(processes=self.parallelism)
+        for _ in range(self.parallelism):
+            pool.apply_async(self.__produce)
+
+        while True:
+
+            gtbox, imgfile, img = self.__data_queue.get()
             h, w, c = img.shape
 
             # clip image
