8d962bfaee1a4146f9a0d550a2b84d1a5bb0bf90
GlassyWing <1490215053@qq.com>
2018-11-12 14:15:16 +0800

    添加文本检测模块

--- text_detection_app.py
+++ text_detection_app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from math import *
 
 import cv2
@@ -29,6 +30,55 @@ def dumpRotateImage(img, degree, pt1, pt2, pt3, pt4):
     return imgOut
 
 
+def sort_box(box):
+    """
+    对box进行排序
+    """
+    box = sorted(box, key=lambda x: sum([x[1], x[3], x[5], x[7]]))
+    return box
+
+
+def single_text_detect(rec, ocr, id_to_char, img, adjust):
+    xDim, yDim = img.shape[1], img.shape[0]
+    xlength = int((rec[2] - rec[0]) * 0.1)
+    ylength = int((rec[3] - rec[1]) * 0.2)
+    if adjust:
+        pt1 = (max(1, rec[0] - xlength), max(1, rec[1] - ylength))
+        pt2 = (rec[2], rec[3])
+        pt3 = (min(rec[6] + xlength, xDim - 2), min(yDim - 2, rec[7] + ylength))
+        pt4 = (rec[4], rec[5])
+    else:
+        pt1 = (max(1, rec[0]), max(1, rec[1]))
+        pt2 = (rec[2], rec[3])
+        pt3 = (min(rec[6], xDim - 2), min(yDim - 2, rec[7]))
+        pt4 = (rec[4], rec[5])
+
+    degree = degrees(atan2(pt2[1] - pt1[1], pt2[0] - pt1[0]))  # 图像倾斜角度
+
+    partImg = dumpRotateImage(img, degree, pt1, pt2, pt3, pt4)
+
+    if partImg.shape[0] < 1 or partImg.shape[1] < 1 or partImg.shape[0] > partImg.shape[1]:  # 过滤异常图片
+        return None
+
+    image = Image.fromarray(partImg).convert('L')
+    text, _ = ocr.predict(image, id_to_char)
+    return text
+
+
+def model(ctpn, ocr, id_to_char, image_path, adjust):
+    text_recs, img = ctpn.predict(image_path, mode=2)
+    text_recs = sort_box(text_recs)
+    results = []
+
+    for index, rec in enumerate(text_recs):
+        text = single_text_detect(rec, ocr, id_to_char, img, adjust)
+
+        if text is not None and len(text) > 0:
+            results.append((rec, text))  # 识别文字
+
+    return results
+
+
 class TextDetectionApp:
 
     def __init__(self,
@@ -54,41 +104,16 @@ class TextDetectionApp:
             self.ocr = DenseNetOCR(num_classes=len(self.id_to_char))
 
     def detect(self, image_path, adjust):
-        text_recs, img = self.ctpn.predict(image_path, mode=2)
-        results = {}
-        xDim, yDim = img.shape[1], img.shape[0]
-
-        for index, rec in enumerate(text_recs):
-            xlength = int((rec[2] - rec[0]) * 0.1)
-            ylength = int((rec[3] - rec[1]) * 0.2)
-            if adjust:
-                pt1 = (max(1, rec[0] - xlength), max(1, rec[1] - ylength))
-                pt2 = (rec[2], rec[1])
-                pt3 = (min(rec[2] + xlength, xDim - 2), min(yDim - 2, rec[3] + ylength))
-                pt4 = (rec[0], rec[3])
-            else:
-                pt1 = (max(1, rec[0]), max(1, rec[1]))
-                pt2 = (rec[2], rec[1])
-                pt3 = (min(rec[2], xDim - 2), min(yDim - 2, rec[3]))
-                pt4 = (rec[0], rec[3])
-
-            degree = degrees(atan2(pt2[1] - pt1[1], pt2[0] - pt1[0]))  # 图像倾斜角度
-
-            partImg = dumpRotateImage(img, degree, pt1, pt2, pt3, pt4)
-
-            if partImg.shape[0] < 1 or partImg.shape[1] < 1 or partImg.shape[0] > partImg.shape[1]:  # 过滤异常图片
-                continue
-
-            image = Image.fromarray(partImg).convert('L')
-            text = self.ocr.predict(image, self.id_to_char)
-
-            if len(text) > 0:
-                results[index] = [rec]
-                results[index].append(text)  # 识别文字
-
-        return results
+        return model(self.ctpn, self.ocr, self.id_to_char, image_path, adjust)
 
 
 if __name__ == '__main__':
-    app = TextDetectionApp(None, None, "data/char_std_5990.txt")
-    print(app.detect("data/demo.jpg", True))
+    app = TextDetectionApp(ctpn_weight_path="model/weights-ctpnlstm-init.hdf5",
+                           densenet_weight_path="model/weights-densent-init.hdf5",
+                           dict_path="data/char_std_5990.txt",
+                           ctpn_config_path="config/ctpn-default.json",
+                           densenet_config_path="config/densent-default.json")
+    start_time = datetime.now()
+    for line_no, line in app.detect("data/demo.jpg", False):
+        print(line)
+    print(f"cost {(datetime.now() - start_time).microseconds / 1000}ms")
