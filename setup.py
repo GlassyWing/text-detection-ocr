61b9d86523c147077520bcba2f163d86df1ec613
manlier <1490215053@qq.com>
2018-12-23 21:33:12 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='dlocr',
 
-      version='0.1',
+      version='0.1-SNAPSHOT',
 
       url='https://github.com/GlassyWing/text-detection-ocr',
 
